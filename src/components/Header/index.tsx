ab672c5519d03177d1e0a684bc1d3ca955aa21a6
Seth Wooten <slwooten14@gmail.com>
2023-05-19 14:24:40 -0400

    header fixed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import PhoneEnabledIcon from '@mui/icons-material/PhoneEnabled';
-import MailIcon from '@mui/icons-material/Mail';
+import { Link } from 'react-router-dom';
 
 // css
 import './header.css';
@@ -11,17 +10,19 @@ import logoSrc from '../../images/dirty-dog-grooming-logo.png';
 const Header = () => {
   return (
     // <div className="header-component">
-      <div className="header-container">
-        {/* <div className="header-icon-container">
+    <div className="header-container">
+      {/* <div className="header-icon-container">
           <MailIcon sx={{ color: "rgb(35, 28, 24)" }} />
         </div> */}
-        <div className="header-logo">
+      <div className="header-logo">
+        <Link to={"/"}>
           <img className='header-logo-image' src={logoSrc} alt="" />
-        </div>
-        {/* <div className="header-icon-container">
+        </Link>
+      </div>
+      {/* <div className="header-icon-container">
           <PhoneEnabledIcon sx={{ color: "rgb(35, 28, 24)" }} />
         </div> */}
-      </div>
+    </div>
     // </div>
   );
 };
